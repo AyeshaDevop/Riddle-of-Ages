9373aded13de48f7fd7761d514bb1780eea103dc
AyeshaDevop <ashiiayesha123@gmail.com>
2024-12-05 13:11:12 +0500

    Done

---
+++ riddle_of_ages.py
@@ -0,0 +1,15 @@
+def main():
+    ayesha = 21
+    fatima = ayesha + 6
+    zara = fatima + 20
+    sara = zara + ayesha
+    moon = zara
+
+    print("Ayesha is " + str(ayesha))
+    print("Fatima is " + str(fatima))
+    print("Zara is " + str(zara))
+    print("Sara is " + str(sara))
+    print("moon is " + str(moon))
+
+if __name__ == '__main__':
+    main()
